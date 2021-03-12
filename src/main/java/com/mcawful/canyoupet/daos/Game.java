dd69a4dd403f1f1ccb0eb6d47ff38b88f3d412fd
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-12 15:04:22 -0500

    Created models needed for data persistence

---
+++ src/main/java/com/mcawful/canyoupet/daos/Game.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.mcawful.canyoupet.daos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.mcawful.canyoupet.models.Animal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * The DAO entity representing the Game object.
+ * 
+ * @author Michael McAuliffe
+ *
+ */
+@Data
+@AllArgsConstructor
+@Entity
+public class Game {
+
+	/**
+	 * The ID of the {@link Game} object.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+
+	/**
+	 * The {@link String} title of the {@link Game} object.
+	 */
+	private String title;
+
+	/**
+	 * The array of {@link Animal} objects in the {@link Game} object.
+	 */
+	private Animal[] animals;
+
+}
