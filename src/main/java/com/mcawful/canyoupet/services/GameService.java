42faca7efbe4bf6f236360af419d61c15529e3ab
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-15 12:10:36 -0400

    Created interface for the GameService

---
+++ src/main/java/com/mcawful/canyoupet/services/GameService.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.mcawful.canyoupet.services;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.mcawful.canyoupet.daos.Game;
+
+/**
+ * A service to handle requests for {@link Game} objects.
+ * 
+ * @author Michael McAuliffe
+ *
+ */
+public interface GameService {
+
+	/**
+	 * Retrieves a {@link Game} object by the {@code titleURI} {@link String} by
+	 * making a call to the appropriate repository.
+	 * 
+	 * @param titleURI the {@code titleURI} {@link String} field of the {@link Game}
+	 *                 object
+	 * @return a {@link Game} object
+	 * @throws EntityNotFoundException when a {@link Game} object cannot be found in
+	 *                                 the repository
+	 */
+	public Game retrieveGameByTitleURI(String titleURI) throws EntityNotFoundException;
+
+}
