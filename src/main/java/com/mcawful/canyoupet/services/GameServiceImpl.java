ce43446ef2fe7e0fbe11c459ab8ed11a4c5b3e75
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-15 12:51:36 -0400

    Create GameServiceImpl.java

---
+++ src/main/java/com/mcawful/canyoupet/services/GameServiceImpl.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mcawful.canyoupet.services;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.mcawful.canyoupet.daos.Game;
+
+/**
+ * @author Michael McAuliffe
+ *
+ */
+public class GameServiceImpl implements GameService {
+
+	@Override
+	public Game retrieveGameByTitleURI(String titleURI) throws EntityNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
