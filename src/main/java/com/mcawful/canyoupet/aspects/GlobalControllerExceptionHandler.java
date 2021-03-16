6a5efce6020d6d55e56ca756fdbce68f8e0d1efe
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-16 14:16:23 -0400

    Create GlobalControllerExceptionHandler.java

---
+++ src/main/java/com/mcawful/canyoupet/aspects/GlobalControllerExceptionHandler.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mcawful.canyoupet.aspects;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Michael McAuliffe
+ *
+ */
+@RestControllerAdvice
+public class GlobalControllerExceptionHandler {
+
+}
