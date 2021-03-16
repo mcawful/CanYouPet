6a34cefe1b03d6b091240300cf2fc7018ca6aed4
michael-alan-mcauliffe <michael.alan.mcauliffe@gmail.com>
2021-03-16 14:07:41 -0400

    Create GameController.java

---
+++ src/main/java/com/mcawful/canyoupet/controllers/GameController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.mcawful.canyoupet.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Michael McAuliffe
+ *
+ */
+@RestController
+public class GameController {
+
+}
